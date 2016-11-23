187a2e41a1895aadc5433878e21f1be4f72876fe
Ian McClellan <im2141@bard.edu>
2016-11-22 20:24:56 -0500

    fixfixfix

--- hours.php
+++ hours.php
@@ -16,7 +16,12 @@ if ($newDates == 1){
 
     $update = "UPDATE hours SET mon = ".$mon.", tue = ".$tue.", wed = ".$wed.", thu = ".$thu.", fri = ".$fri." WHERE officeHours = 1";
 
-    $result = mysqli_query($link, $update);
+    //$result = mysqli_query($link, $update);
+
+    if($connection){
+        if(mysqli_query($link, $update) === TRUE){
+        }
+    }
 }
 else {
     $query = "SELECT * FROM hours WHERE officeHours = 1";   // The only row in the 'hours' table contains the current office hours
